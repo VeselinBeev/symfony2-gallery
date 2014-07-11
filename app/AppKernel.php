30483fc9a8104d37bfe9cd42976f11f049cf4d56
VeselinBeev <veselinbeev@gmail.com>
2014-07-11 16:21:31 +0400

    Project

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,10 +16,13 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new SymfonyGallery\AdminCoreBundle\AdminCoreBundle(),
+            new SymfonyGallery\FrontBundle\FrontBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new SymfonyGallery\UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
---
+++ src/SymfonyGallery/AdminCoreBundle/AdminCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SymfonyGallery\AdminCoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AdminCoreBundle extends Bundle
+{
+}
